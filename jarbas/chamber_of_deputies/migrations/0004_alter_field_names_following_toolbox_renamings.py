0bd0ed17c76ec566b893772ae5db903bfaa996a4
Irio Musskopf <irio@protonmail.com>
2018-08-24 13:22:04 +0200

    Migrate data and fields

--- jarbas/chamber_of_deputies/migrations/0004_alter_field_names_following_toolbox_renamings.py
+++ jarbas/chamber_of_deputies/migrations/0004_alter_field_names_following_toolbox_renamings.py
@@ -2,20 +2,43 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import migrations, models
 
 
+def convert_reimbursement_numbers_to_array(apps, schema_editor):
+    Reimbursement = apps.get_model("chamber_of_deputies", "Reimbursement")
+    for record in Reimbursement.objects.all():
+        record.numbers = record.reimbursement_numbers.split(", ")
+        record.save()
+
+
+def convert_reimbursement_numbers_to_array_rollback(apps, schema_editor):
+    Reimbursement = apps.get_model("chamber_of_deputies", "Reimbursement")
+    for record in Reimbursement.objects.all():
+        record.reimbursement_numbers = ", ".join(record.numbers)
+        record.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('chamber_of_deputies', '0003_remove_available_in_latest_dataset_field'),
+        ("chamber_of_deputies", "0003_remove_available_in_latest_dataset_field")
     ]
 
     operations = [
         migrations.RenameField(
-            model_name='reimbursement',
-            old_name='total_reimbursement_value',
-            new_name='total_value',
+            model_name="reimbursement",
+            old_name="total_reimbursement_value",
+            new_name="total_value",
         ),
+        migrations.AddField(
+            model_name="reimbursement",
+            name="numbers",
+            field=ArrayField(models.CharField(max_length=128), default=list),
+        ),
+        migrations.RunPython(
+            convert_reimbursement_numbers_to_array,
+            convert_reimbursement_numbers_to_array_rollback,
+        ),
+        migrations.RemoveField(model_name="reimbursement", name="net_values"),
         migrations.RemoveField(
-            model_name='reimbursement',
-            name='net_values',
+            model_name="reimbursement", name="reimbursement_numbers"
         ),
     ]
